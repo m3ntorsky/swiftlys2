d440327f5df2b091c9dc9d81e773700aecc01ece
skuzzis <andreieurt@gmail.com>
2025-09-04 10:53:11 +0300

    feat(api): VFunc Call

---
+++ src/api/memory/virtual/call.h
@@ -0,0 +1,30 @@
+/************************************************************************************************
+ *  SwiftlyS2 is a scripting framework for Source2-based games.
+ *  Copyright (C) 2025 Swiftly Solution SRL via Sava Andrei-Sebastian and it's contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ************************************************************************************************/
+
+#ifndef src_api_memory_virtual_call_h
+#define src_api_memory_virtual_call_h
+
+template<class T, class... Args>
+constexpr T CallVFunc(int iIndex, void* pThis, Args... args) noexcept
+{
+    return reinterpret_cast<T(*)(void*, Args...)>(reinterpret_cast<void***>(pThis)[0][iIndex])(pThis, args...);
+}
+
+#define CALL_VIRTUAL(retType, idx, ...) CallVFunc<retType>(idx, __VA_ARGS__)
+
+#endif
