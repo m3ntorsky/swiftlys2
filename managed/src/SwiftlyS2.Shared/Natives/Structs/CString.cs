8bc28640736d8a1a758ec06ab8e9d13ab5347013
samyyc <3356207189@qq.com>
2025-09-13 01:38:22 +0800

    feat(managed): Add native types

--- managed/src/SwiftlyS2.Shared/Natives/Structs/CString.cs
+++ managed/src/SwiftlyS2.Shared/Natives/Structs/CString.cs
@@ -2,6 +2,7 @@ using System.Buffers;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using SwiftlyS2.Core.Extensions;
 
 namespace SwiftlyS2.Shared.Natives;
 
@@ -9,37 +10,37 @@ namespace SwiftlyS2.Shared.Natives;
 /// Wrapper class for native char*.
 /// </summary>
 [StructLayout(LayoutKind.Explicit, Size = 8)]
-public struct CString {
+public ref struct CString {
 
   private static List<nint> _allocated = new List<nint>();
 
   [FieldOffset(0)]
-  private nint _ptr;
+  private nint _pString; // char*
   
   public string Value {
     get {
-      if (_ptr == nint.Zero) {
+      if (!_pString.IsValidPtr()) {
         return string.Empty;
       }
-      return Marshal.PtrToStringUTF8(_ptr)!;
+      return Marshal.PtrToStringUTF8(_pString)!;
     }
 
     set {
-      if (_ptr == nint.Zero) {
+      if (_pString == nint.Zero) {
         // maybe a warning here
       }
       unsafe {
-        if (_allocated.Contains(_ptr)) {
-          NativeMemory.Free(_ptr.ToPointer());
-          _allocated.Remove(_ptr);
+        if (_allocated.Contains(_pString)) {
+          NativeMemory.Free(_pString.ToPointer());
+          _allocated.Remove(_pString);
         }
         var bytes = Encoding.UTF8.GetBytes(value);
-        _ptr = (nint)NativeMemory.Alloc((nuint)bytes.Length + 1);
+        _pString = (nint)NativeMemory.Alloc((nuint)bytes.Length + 1);
         fixed (byte* p = bytes) {
-          NativeMemory.Copy(p, (byte*)_ptr, (nuint)bytes.Length);
+          NativeMemory.Copy(p, (byte*)_pString, (nuint)bytes.Length);
         }
-        ((byte*)_ptr)[bytes.Length] = 0;
-        _allocated.Add(_ptr);
+        ((byte*)_pString)[bytes.Length] = 0;
+        _allocated.Add(_pString);
       }
     }
   }
--- managed/src/SwiftlyS2.Shared/Natives/Structs/Vector.cs
+++ managed/src/SwiftlyS2.Shared/Natives/Structs/Vector.cs
@@ -27,6 +27,16 @@ public struct Vector {
     Z = other.Z;
   }
 
+  public System.Numerics.Vector3 ToBuiltin()
+  {
+    return new(X, Y, Z);
+  }
+
+  public static Vector FromBuiltin(System.Numerics.Vector3 vector)
+  {
+    return new(vector.X, vector.Y, vector.Z);
+  }
+
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public float Length() => (float)Math.Sqrt(X * X + Y * Y + Z * Z);
 
@@ -44,6 +54,39 @@ public struct Vector {
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public Vector Cross(Vector other) => new(Y * other.Z - Z * other.Y, Z * other.X - X * other.Z, X * other.Y - Y * other.X);
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static float Dot(Vector a, Vector b) => a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public float Dot(Vector other) => Dot(this, other);
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public void Normalize() {
+    var len = Length();
+    if (len > 0f) {
+      var inv = 1.0f / len;
+      X *= inv;
+      Y *= inv;
+      Z *= inv;
+    }
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public Vector Normalized() {
+    var len = Length();
+    if (len > 0f) {
+      var inv = 1.0f / len;
+      return new(X * inv, Y * inv, Z * inv);
+    }
+    return Zero;
+  }
+
+  public void Deconstruct(out float x, out float y, out float z) {
+    x = X;
+    y = Y;
+    z = Z;
+  }
 
 
   public override bool Equals(object? obj) => obj is Vector vector && this == vector;
@@ -72,7 +115,16 @@ public struct Vector {
   public static Vector operator *(Vector a, float b) => new(a.X * b, a.Y * b, a.Z * b);
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public static Vector operator /(Vector a, float b) => new(a.X / b, a.Y / b, a.Z / b);
+  public static Vector operator *(float b, Vector a) => new(a.X * b, a.Y * b, a.Z * b);
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static Vector operator /(Vector a, float b) {
+    if (b == 0) {
+      throw new DivideByZeroException();
+    }
+    var oofl = 1.0f / b;
+    return new(a.X * oofl, a.Y * oofl, a.Z * oofl);
+  }
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static Vector operator -(Vector a) => new(-a.X, -a.Y, -a.Z);
