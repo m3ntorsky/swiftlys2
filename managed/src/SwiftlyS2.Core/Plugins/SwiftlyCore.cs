3455048e7088ee947aa33859f4b5a99814add4ac
samyyc <3356207189@qq.com>
2025-09-13 14:55:20 +0800

    fix(managed): Fix core dispose

--- managed/src/SwiftlyS2.Core/Plugins/SwiftlyCore.cs
+++ managed/src/SwiftlyS2.Core/Plugins/SwiftlyCore.cs
@@ -10,6 +10,8 @@ namespace SwiftlyS2.Core.Plugins;
 
 internal class SwiftlyCore : ISwiftlyCore, IDisposable {
 
+  private ServiceProvider _ServiceProvider { get; init; }
+
   public EventSubscriber CustomEventSubscriber { get; init; }
 
   public PluginConfigurationService Configuration { get; init; }
@@ -21,19 +23,19 @@ internal class SwiftlyCore : ISwiftlyCore, IDisposable {
 
     ServiceCollection services = new();
 
-    services.AddSingleton<PluginId>(id);
+    services.AddSingleton(id);
     services.AddSingleton(coreProvider.GetRequiredService<ProfileService>());
     services.AddSingleton(coreProvider.GetRequiredService<ConfigurationService>());
     services.AddSingleton<EventSubscriber>();
     services.AddSingleton<PluginConfigurationService>();
 
-    var provider = services.BuildServiceProvider();
-    CustomEventSubscriber = provider.GetRequiredService<EventSubscriber>();
-    Configuration = provider.GetRequiredService<PluginConfigurationService>();
+    _ServiceProvider = services.BuildServiceProvider();
+    CustomEventSubscriber = _ServiceProvider.GetRequiredService<EventSubscriber>();
+    Configuration = _ServiceProvider.GetRequiredService<PluginConfigurationService>();
   }
 
   public void Dispose() {
-    CustomEventSubscriber.Dispose();
+    _ServiceProvider.Dispose();
   }
 
   IEventSubscriber ISwiftlyCore.CustomEvents => CustomEventSubscriber;
