9f4c807ae362680ae8e20bb0b6b38adf090c9749
skuzzis <andreieurt@gmail.com>
2025-09-15 11:20:10 +0300

    feat(bridge): Voice Manager

---
+++ managed/src/SwiftlyS2.Generated/Natives/VoiceManager.cs
@@ -0,0 +1,27 @@
+#pragma warning disable CS0649
+
+using System.Buffers;
+using System.Text;
+
+namespace SwiftlyS2.Core.Natives;
+
+internal static class NativeVoiceManager {
+  private unsafe static delegate* unmanaged<int, int, int, void> _SetClientListenOverride;
+  public unsafe static void SetClientListenOverride(int playerid, int targetid, int listenOverride) {
+    _SetClientListenOverride(playerid, targetid, listenOverride);
+  }
+  private unsafe static delegate* unmanaged<int, int, int> _GetClientListenOverride;
+  public unsafe static int GetClientListenOverride(int playerid, int targetid) {
+    var ret = _GetClientListenOverride(playerid, targetid);
+    return ret;
+  }
+  private unsafe static delegate* unmanaged<int, int, void> _SetClientVoiceFlags;
+  public unsafe static void SetClientVoiceFlags(int playerid, int flags) {
+    _SetClientVoiceFlags(playerid, flags);
+  }
+  private unsafe static delegate* unmanaged<int, int> _GetClientVoiceFlags;
+  public unsafe static int GetClientVoiceFlags(int playerid) {
+    var ret = _GetClientVoiceFlags(playerid);
+    return ret;
+  }
+}
---
+++ src/scripting/engine/voicemanager.cpp
@@ -0,0 +1,49 @@
+/************************************************************************************************
+ * SwiftlyS2 is a scripting framework for Source2-based games.
+ * Copyright (C) 2025 Swiftly Solution SRL via Sava Andrei-Sebastian and it's contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ************************************************************************************************/
+
+#include <api/interfaces/manager.h>
+#include <scripting/scripting.h>
+
+void Bridge_VoiceManager_SetClientListenOverride(int playerid, int targetid, int override)
+{
+    auto voicemanager = g_ifaceService.FetchInterface<IVoiceManager>(VOICEMANAGER_INTERFACE_VERSION);
+    voicemanager->SetClientListenOverride(playerid, targetid, (ListenOverride)override);
+}
+
+int Bridge_VoiceManager_GetClientListenOverride(int playerid, int targetid)
+{
+    auto voicemanager = g_ifaceService.FetchInterface<IVoiceManager>(VOICEMANAGER_INTERFACE_VERSION);
+    return static_cast<int>(voicemanager->GetClientListenOverride(playerid, targetid));
+}
+
+void Bridge_VoiceManager_SetClientVoiceFlags(int playerid, int flags)
+{
+    auto voicemanager = g_ifaceService.FetchInterface<IVoiceManager>(VOICEMANAGER_INTERFACE_VERSION);
+    voicemanager->SetClientVoiceFlags(playerid, (VoiceFlagValue)flags);
+}
+
+int Bridge_VoiceManager_GetClientVoiceFlags(int playerid)
+{
+    auto voicemanager = g_ifaceService.FetchInterface<IVoiceManager>(VOICEMANAGER_INTERFACE_VERSION);
+    return static_cast<int>(voicemanager->GetClientVoiceFlags(playerid));
+}
+
+DEFINE_NATIVE("VoiceManager.SetClientListenOverride", Bridge_VoiceManager_SetClientListenOverride);
+DEFINE_NATIVE("VoiceManager.GetClientListenOverride", Bridge_VoiceManager_GetClientListenOverride);
+DEFINE_NATIVE("VoiceManager.SetClientVoiceFlags", Bridge_VoiceManager_SetClientVoiceFlags);
+DEFINE_NATIVE("VoiceManager.GetClientVoiceFlags", Bridge_VoiceManager_GetClientVoiceFlags);
