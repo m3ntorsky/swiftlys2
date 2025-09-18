defc8a754a6ab4dcb42c33faee06124d79f975b9
skuzzis <andreieurt@gmail.com>
2025-09-18 19:51:12 +0300

    feat(managed): CRecipientFilter

---
+++ managed/src/SwiftlyS2.Shared/Natives/Structs/CBitVec.cs
@@ -0,0 +1,126 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SwiftlyS2.Shared.Natives;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct CBitVec
+{
+    public uint[] Ints;
+
+    public CBitVec(uint bits)
+    {
+        Ints = new uint[(bits + (32 - 1)) / 32];
+    }
+
+    public bool IsFixedSize() => true;
+    public uint NumDWords() => (uint)Ints.Length;
+    public uint GetNumBits() => (uint)(Ints.Length * 32 - 31);
+
+    public void ClearAll()
+    {
+        for (int i = 0; i < Ints.Length; i++) Ints[i] = 0;
+    }
+
+    public void SetAll()
+    {
+        for (int i = 0; i < Ints.Length; i++) Ints[i] = uint.MaxValue;
+    }
+
+    public void Set(uint index)
+    {
+        if ((index >> 5) > Ints.Length) throw new IndexOutOfRangeException($"The index provided is out of range, it goes to Ints[{index >> 5}], but the limit is {Ints.Length}");
+
+        unsafe
+        {
+            fixed (uint* ptr = Ints)
+            {
+                Ints[index >> 5] |= (uint)(1 << ((int) index & 31));
+            }
+        }
+    }
+
+    public void Set(int index)
+    {
+        if ((index >> 5) > Ints.Length) throw new IndexOutOfRangeException($"The index provided is out of range, it goes to Ints[{index >> 5}], but the limit is {Ints.Length}");
+
+        unsafe
+        {
+            fixed (uint* ptr = Ints)
+            {
+                Ints[index >> 5] |= (uint)(1 << (index & 31));
+            }
+        }
+    }
+
+    public void Clear(uint index)
+    {
+        if ((index >> 5) > Ints.Length) throw new IndexOutOfRangeException($"The index provided is out of range, it goes to Ints[{index >> 5}], but the limit is {Ints.Length}");
+
+        unsafe
+        {
+            fixed (uint* ptr = Ints)
+            {
+                Ints[index >> 5] &= ~(uint)(1 << ((int)index & 31));
+            }
+        }
+    }
+
+    public void Clear(int index)
+    {
+        if ((index >> 5) > Ints.Length) throw new IndexOutOfRangeException($"The index provided is out of range, it goes to Ints[{index >> 5}], but the limit is {Ints.Length}");
+
+        unsafe
+        {
+            fixed (uint* ptr = Ints)
+            {
+                Ints[index >> 5] &= ~(uint)(1 << (index & 31));
+            }
+        }
+    }
+
+    public bool IsSet(uint index)
+    {
+        if ((index >> 5) > Ints.Length) throw new IndexOutOfRangeException($"The index provided is out of range, it goes to Ints[{index >> 5}], but the limit is {Ints.Length}");
+
+        unsafe
+        {
+            fixed (uint* ptr = Ints)
+            {
+                return (Ints[index >> 5] & ((uint)(1 << ((int)index & 31)))) != 0;
+            }
+        }
+    }
+
+    public bool IsSet(int index)
+    {
+        if ((index >> 5) > Ints.Length) throw new IndexOutOfRangeException($"The index provided is out of range, it goes to Ints[{index >> 5}], but the limit is {Ints.Length}");
+
+        unsafe
+        {
+            fixed (uint* ptr = Ints)
+            {
+                return (Ints[index >> 5] & ((uint)(1 << (index & 31)))) != 0;
+            }
+        }
+    }
+
+    public int Count()
+    {
+        int count = 0;
+        for (int i = 0; i < Ints.Length; i++)
+        {
+            uint v = Ints[i];
+            while (v != 0)
+            {
+                v &= v - 1;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public bool IsAllClear()
+    {
+        return Count() == 0;
+    }
+};
---
+++ managed/src/SwiftlyS2.Shared/Natives/Structs/CRecipientFilter.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SwiftlyS2.Shared.Natives;
+
+public enum NetChannelBufType_t: sbyte
+{
+    BUF_DEFAULT = -1,
+    BUF_UNRELIABLE = 0,
+    BUF_RELIABLE,
+    BUF_VOICE,
+};
+
+[StructLayout(LayoutKind.Sequential)]
+public class CRecipientFilter
+{
+    public NetChannelBufType_t BufferType;
+    public bool InitMessage;
+    public CBitVec RecipientFilters;
+
+    public CRecipientFilter(NetChannelBufType_t BufType = NetChannelBufType_t.BUF_RELIABLE, bool bInitMessage = false)
+    {
+        RecipientFilters = new(64);
+        InitMessage = bInitMessage;
+        BufferType = BufType;
+    }
+
+    public virtual void NopDestructor() { }
+
+    public virtual NetChannelBufType_t GetNetworkBufferType() => BufferType;
+    public virtual bool IsInitMessage() => InitMessage;
+    public virtual ref CBitVec GetRecipients() => ref RecipientFilters;
+
+    public void AddAllPlayers()
+    {
+        // @todo: When playermanager will be implemeneted, iterate through all the 64 players and if they're online and are authorized, add them here
+    }
+
+    public void RemoveAllPlayers()
+    {
+        RecipientFilters.ClearAll();
+    }
+
+    public void AddRecipient(int playerid)
+    {
+        if (playerid < 0 || playerid > 63) throw new IndexOutOfRangeException("PlayerID out of range (0-63).");
+
+        RecipientFilters.Set(playerid);
+    }
+
+    public void RemoveRecipient(int playerid)
+    {
+        if (playerid < 0 || playerid > 63) throw new IndexOutOfRangeException("PlayerID out of range (0-63).");
+
+        RecipientFilters.Clear(playerid);
+    }
+
+    public int GetRecipientCount()
+    {
+        return RecipientFilters.Count();
+    }
+}
