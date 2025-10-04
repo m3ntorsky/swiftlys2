64c60bc270ba039db8ca06faebf9f0643e946ccc
samyyc <s1myyc@outlook.com>
2025-10-04 17:51:23 +0800

    feat(managed): Translation color

--- managed/src/SwiftlyS2.Core/Modules/Translations/TranslationFactory.cs
+++ managed/src/SwiftlyS2.Core/Modules/Translations/TranslationFactory.cs
@@ -35,6 +35,9 @@ internal class TranslationFactory
       };
 
       var translation = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(translationFile), options) ?? new();
+      foreach (var translationEntry in translation) {
+        translation[translationEntry.Key] = ReplaceColor(translationEntry.Value);
+      }
       resource.Resources[new Language(language)] = translation;
     }
 
@@ -48,6 +51,41 @@ internal class TranslationFactory
 
     return resource;
   }
+
+  private static readonly Dictionary<string, string> ColorCodes = new()
+  {
+    { "[default]", "\x01" },
+    { "[/]", "\x01" },
+    { "[white]", "\x01" },
+    { "[darkred]", "\x02" },
+    { "[lightpurple]", "\x03" },
+    { "[green]", "\x04" },
+    { "[olive]", "\x05" },
+    { "[lime]", "\x06" },
+    { "[red]", "\x07" },
+    { "[gray]", "\x08" },
+    { "[grey]", "\x08" },
+    { "[lightyellow]", "\x09" },
+    { "[yellow]", "\x09" },
+    { "[silver]", "\x0A" },
+    { "[bluegrey]", "\x0A" },
+    { "[lightblue]", "\x0B" },
+    { "[blue]", "\x0B" },
+    { "[darkblue]", "\x0C" },
+    { "[purple]", "\x0E" },
+    { "[magenta]", "\x0E" },
+    { "[lightred]", "\x0F" },
+    { "[gold]", "\x10" },
+    { "[orange]", "\x10" }
+  };
+
+  private static string ReplaceColor(string original) {
+    foreach (var color in ColorCodes) {
+      original = original.Replace(color.Key, color.Value);
+    }
+    return original;
+  }
+
 
   public static Localizer CreateLocalizer(TranslationResource resource, Language language) {
 
