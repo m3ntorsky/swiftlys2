3d098ae1547155fc366b401e63b1579d7a5dc7ed
skuzzis <andreieurt@gmail.com>
2025-10-12 21:27:34 +0300

    feat(helpers): ChatColors enum

--- managed/src/SwiftlyS2.Shared/Helper.cs
+++ managed/src/SwiftlyS2.Shared/Helper.cs
@@ -2,7 +2,8 @@ using SwiftlyS2.Shared.Schemas;
 
 namespace SwiftlyS2.Shared;
 
-public static class Helper {
+public static class Helper
+{
 
   private static readonly Dictionary<string, string> ColorCodes = new()
   {
@@ -31,16 +32,43 @@ public static class Helper {
     { "[orange]", "\x10" }
   };
 
+  public enum ChatColors
+  {
+    Default = "[/]",
+    White = "[white]",
+    DarkRed = "[darkred]",
+    Green = "[green]",
+    LightYellow = "[lightyellow]",
+    LightBlue = "[lightblue]",
+    Olive = "[olive]",
+    Lime = "[lime]",
+    Red = "[red]",
+    LightPurple = "[lightpurple]",
+    Purple = "[purple]",
+    Grey = "[grey]",
+    Yellow = "[yellow]",
+    Gold = "[gold]",
+    Silver = "[silver]",
+    Blue = "[blue]",
+    DarkBlue = "[darkblue]",
+    BlueGrey = "[bluegrey]",
+    Magenta = "[magenta]",
+    LightRed = "[lightred]",
+    Orange = "[orange]",
+  }
+
   /// <summary>
   /// Replace the color codes in the text with the corresponding color codes.
   /// </summary>
   /// <param name="text">The text to replace the color codes in.</param>
   /// <returns>The text with the color codes replaced.</returns>
-  public static string Colored(this string text) {
+  public static string Colored(this string text)
+  {
     if (text.StartsWith("["))
       text = " " + text;
-      
-    foreach (var color in ColorCodes) {
+
+    foreach (var color in ColorCodes)
+    {
       text = text.Replace(color.Key, color.Value);
     }
 
@@ -53,7 +81,8 @@ public static class Helper {
   /// <typeparam name="T">The schema class to convert to.</typeparam>
   /// <param name="ptr">The pointer to the schema class.</param>
   /// <returns>The schema class.</returns>
-  public static T AsSchema<T>(nint ptr) where T : ISchemaClass<T> {
+  public static T AsSchema<T>(nint ptr) where T : ISchemaClass<T>
+  {
     return T.From(ptr);
   }
 }
