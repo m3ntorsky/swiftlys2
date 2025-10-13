98c8054f2e307383bb220475f731d5938a277da3
skuzzis <andreieurt@gmail.com>
2025-10-13 11:39:17 +0300

    chore(CUtlMap): Make it support other index kinds

--- managed/src/SwiftlyS2.Shared/Natives/Structs/CUtlMap.cs
+++ managed/src/SwiftlyS2.Shared/Natives/Structs/CUtlMap.cs
@@ -1,45 +1,47 @@
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 namespace SwiftlyS2.Shared.Natives;
 
 [StructLayout(LayoutKind.Sequential)]
-public struct CUtlMap<TKey, TValue> : IDisposable
+public struct CUtlMap<TKey, TValue, TIndex> : IDisposable
     where TKey : unmanaged
     where TValue : unmanaged
+    where TIndex : unmanaged, IBinaryInteger<TIndex>, IMinMaxValue<TIndex>
 {
-    public CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, int> Tree;
+    public CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, TIndex> Tree;
 
-    public CUtlMap(int growSize, int initSize, CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, int>.LessFunc func)
+    public CUtlMap(TIndex growSize, TIndex initSize, CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, TIndex>.LessFunc func)
     {
-        Tree = new CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, int>(growSize, initSize, func);
+        Tree = new CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, TIndex>(growSize, initSize, func);
     }
 
-    public CUtlMap(CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, int>.LessFunc func)
+    public CUtlMap(CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, TIndex>.LessFunc func)
     {
-        Tree = new CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, int>(func);
+        Tree = new CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, TIndex>(func);
     }
 
-    public void EnsureCapacity(int num)
+    public void EnsureCapacity(TIndex num)
     {
         Tree.EnsureCapacity(num);
     }
 
-    public ref TValue this[int idx] => ref Tree[idx].Element;
-    public ref TKey Key(int idx) => ref Tree[idx].Key;
+    public ref TValue this[TIndex idx] => ref Tree[idx].Element;
+    public ref TKey Key(TIndex idx) => ref Tree[idx].Key;
     public int Count => (int)Tree.Count;
-    public int MaxElement => (int)Tree.MaxElement;
-    public bool IsValidIndex(int idx) => Tree.IsValidIndex(idx);
+    public TIndex MaxElement => Tree.MaxElement;
+    public bool IsValidIndex(TIndex idx) => Tree.IsValidIndex(idx);
     public bool IsValid() => Tree.IsValid();
-    public int InvalidIndex() => Tree.InvalidIndex();
-    public void SetLessFunc(CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, int>.LessFunc func) => Tree.SetLessFunc(func);
-    public int Insert(TKey key, TValue element) => Tree.Insert(new CUtlMapTreeNode<TKey, TValue> { Key = key, Element = element });
-    public int Insert(TKey key) => Tree.Insert(new CUtlMapTreeNode<TKey, TValue> { Key = key });
-    public int Find(TKey key)
+    public TIndex InvalidIndex() => Tree.InvalidIndex();
+    public void SetLessFunc(CUtlRBTree<CUtlMapTreeNode<TKey, TValue>, TIndex>.LessFunc func) => Tree.SetLessFunc(func);
+    public TIndex Insert(TKey key, TValue element) => Tree.Insert(new CUtlMapTreeNode<TKey, TValue> { Key = key, Element = element });
+    public TIndex Insert(TKey key) => Tree.Insert(new CUtlMapTreeNode<TKey, TValue> { Key = key });
+    public TIndex Find(TKey key)
     {
         var node = new CUtlMapTreeNode<TKey, TValue> { Key = key };
         return Tree.Find(node);
     }
-    public void RemoveAt(int idx) => Tree.RemoveAt(idx);
+    public void RemoveAt(TIndex idx) => Tree.RemoveAt(idx);
     public bool Remove(TKey key)
     {
         var node = new CUtlMapTreeNode<TKey, TValue> { Key = key };
@@ -47,18 +49,18 @@ public struct CUtlMap<TKey, TValue> : IDisposable
     }
     public void RemoveAll() => Tree.RemoveAll();
     public void Purge() => Tree.Purge();
-    public int FirstInOrdered() => Tree.FirstInorder();
-    public int NextInOrdered(int idx) => Tree.NextInorder(idx);
-    public int PrevInOrdered(int idx) => Tree.PrevInorder(idx);
-    public int LastInOrdered() => Tree.LastInorder();
+    public TIndex FirstInOrdered() => Tree.FirstInorder();
+    public TIndex NextInOrdered(TIndex idx) => Tree.NextInorder(idx);
+    public TIndex PrevInOrdered(TIndex idx) => Tree.PrevInorder(idx);
+    public TIndex LastInOrdered() => Tree.LastInorder();
 
-    public void Reinsert(TKey key, int idx)
+    public void Reinsert(TKey key, TIndex idx)
     {
         Tree[idx].Key = key;
         Tree.Reinsert(idx);
     }
 
-    public int InsertOrReplace(TKey key, TValue element)
+    public TIndex InsertOrReplace(TKey key, TValue element)
     {
         var node = new CUtlMapTreeNode<TKey, TValue> { Key = key };
         var idx = Tree.Find(node);
